d6464bef3b55740f918c9e29889fe9ad3127c8d6
Mohan Kumar <mohankumar.batchu@gmail.com>
2017-09-29 17:56:23 +0530

    updated geko driver

--- src/test/java/VisionUtilities/VisionWebFunctions.java
+++ src/test/java/VisionUtilities/VisionWebFunctions.java
@@ -9,32 +9,33 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class VisionWebFunctions {
 	
 	
-	static WebDriver webdriver = new FirefoxDriver();
+	static WebDriver webdriver = null;
 	static String pageTitle = null;
 	static String pageUrl = null;
 	static String getText = null;
 	
 	public static void Login() throws Exception{
 		
+		System.setProperty("webdriver.gecko.driver", "/Users/mohankumar/Documents/AdtechAutomation/VisionWebAutomation/Drivers/geckodriver");
+		webdriver = new FirefoxDriver();
 		
 		webdriver.get("http://136.179.23.98/default.aspx");
 		webdriver.manage().window().maximize();
 		
-		Thread.sleep(30000);
 		
-		WebDriverWait name = new WebDriverWait(webdriver,50);
+		WebDriverWait name = new WebDriverWait(webdriver,20);
 		name.until(ExpectedConditions.presenceOfElementLocated(By.id("UserName")));  
 		
 		webdriver.findElement(By.id("UserName")).sendKeys("testadmin");
 		System.out.println("Batchu");
 		
-		WebDriverWait pass = new WebDriverWait(webdriver,50);
+		WebDriverWait pass = new WebDriverWait(webdriver,20);
 		pass.until(ExpectedConditions.presenceOfElementLocated(By.id("PassWord"))); 
 	    
 		webdriver.findElement(By.id("PassWord")).sendKeys("Breg2007!");
 		System.out.println("Kavya");
 		
-		WebDriverWait login = new WebDriverWait(webdriver,50);
+		WebDriverWait login = new WebDriverWait(webdriver,20);
 		login.until(ExpectedConditions.presenceOfElementLocated(By.id("Login")));
 		
 		webdriver.findElement(By.id("Login")).click();
